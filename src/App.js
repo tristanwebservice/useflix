f5f4d47a54ca86102edcab17ee578b7d154081b7
tristanwebservice <198068068+tristanwebservice@users.noreply.github.com>
2025-10-22 13:27:48 +0200

    feat: implement selected movie details loading

--- src/App.js
+++ src/App.js
@@ -111,15 +111,6 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
 
-      <StarRating
-        size={30}
-        color="yellow"
-        className="test"
-        maxRating={10}
-        messages={[]}
-        defaultRating={0}
-        onSetRating={setMovieRating}
-      />
       <p>this movie was rated as {movieRating}</p>
 
       <Main>
@@ -232,12 +223,83 @@ function Movie({ movie, onSelectMovie }) {
 }
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  useEffect(
+    function () {
+      async function GetMovieDetails() {
+        setIsLoading(true);
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${movieKey}&i=${selectedId}`
+        );
+        const data = await res.json();
+        setMovie(data);
+        setIsLoading(false);
+      }
+      GetMovieDetails();
+    },
+    [selectedId]
+  );
+
   return (
     <div className="details">
-      <button className="btn-back" onClick={onCloseMovie}>
-        &larr;
-      </button>
-      {selectedId}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <header>
+            <button className="btn-back" onClick={onCloseMovie}>
+              &larr;
+            </button>
+            <img src={poster} alt={`Poster of ${movie}`} />
+            <div className="details-overview">
+              <h2>{title}</h2>
+              <p>
+                {released} &bull; {runtime}
+              </p>
+              <p>{genre}</p>
+              <p>
+                <span>⭐</span>
+                {imdbRating}
+              </p>
+            </div>
+          </header>
+
+          <section>
+            <div className="rating">
+              <StarRating
+                size={24}
+                color="yellow"
+                className="test"
+                maxRating={10}
+                messages={[]}
+                defaultRating={0}
+                // onSetRating={setMovieRating}
+              />
+            </div>
+
+            <p>
+              <em>{plot}</em>
+            </p>
+            <p>Starring {actors}</p>
+            <p>Directed by {director}</p>
+          </section>
+        </>
+      )}
     </div>
   );
 }
