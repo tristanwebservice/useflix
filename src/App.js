bb50bb220419f27d965a9ad03ae898a1bd3af3f9
tristanwebservice <198068068+tristanwebservice@users.noreply.github.com>
2025-10-24 13:14:55 +0200

    refactor: implement custom useLocalStorageState hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 import StarRating from "./StarRating";
 import { useMovies } from "./useMovies";
+import { useLocalStorageState } from "./useLocalStorageState";
 
 const movieKey = `f84fc31d`; // bad practice! public key used here for demo purposes
 
@@ -11,12 +12,10 @@ export default function App() {
   const [query, setQuery] = useState(``);
   const [selectedId, setSelectedId] = useState(null);
   const { movies, isLoading, error } = useMovies(query);
-  const [movieRating, setMovieRating] = useState();
 
-  const [watched, setWatched] = useState(() => {
-    const storedValue = localStorage.getItem(`watched`);
-    return storedValue ? JSON.parse(storedValue) : [];
-  });
+  const [watched, setWatched] = useLocalStorageState([], `watched`);
+
+  const [movieRating, setMovieRating] = useState();
 
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
@@ -36,13 +35,6 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
-
-  useEffect(
-    function () {
-      localStorage.setItem(`watched`, JSON.stringify(watched));
-    },
-    [watched]
-  );
 
   return (
     <>
