c35317cbff18a6458de98f14fbcdcafab5121f00
tristanwebservice <198068068+tristanwebservice@users.noreply.github.com>
2025-10-24 11:05:48 +0200

    refactor: implement abort controller for data fetching

--- src/App.js
+++ src/App.js
@@ -99,8 +99,8 @@ export default function App() {
         setMovies(data.Search);
         setError(``);
       } catch (err) {
-        console.error(err.message);
         if (err.name !== `AbortError`) {
+          console.log(err.message);
           setError(err.message);
         }
       } finally {
@@ -113,7 +113,7 @@ export default function App() {
       setError(``);
       return;
     }
-
+    handleCloseMovie();
     fetchMovies();
 
     return function () {
@@ -278,6 +278,17 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     onAddWatched(newWatchedMovie);
     onCloseMovie();
   }
+
+  useEffect(() => {
+    const callback = (e) => {
+      if (e.code === `Escape`) {
+        onCloseMovie();
+      }
+    };
+
+    document.addEventListener(`keydown`, callback);
+    return () => document.removeEventListener(`keydown`, callback);
+  }, [onCloseMovie]);
 
   useEffect(
     function () {
